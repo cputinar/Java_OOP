146ca936b01f5fa7cda3cba81a71d236135d4956
mariajyu <mariajyu@gmail.com>
2014-02-10 00:03:51 -0800

    Update SeqIt.java
    updated last part

--- part3/SeqIt.java
+++ part3/SeqIt.java
@@ -84,12 +84,27 @@ protected class JumbleIt extends SeqIt {
 
 protected class JumbleUser extends JumbleIt {
 	public static int lengthLongestNDCSS1(Jumble j) {
-	int count = 0;
-	int temp = s.next();
-	int temp2 = s.next();
-	if (temp1 <= temp2)
-		count++;
-	else
-		return count;
+	int temp = 1;
+	int longest = 0;
+
+	if(j.hasNext) {
+		int holder1 = j.next();
+		longest = temp;
+	}
+
+	while (j.hasNext) {
+		int holder2 = j.next();
+		if( holder2>= holder1)
+			temp++;
+		else:
+			if(temp > longest)
+				longest = temp;
+			temp = 1;
+		holder1=holder2;
+
+
+	return longest;
+
+		}
 	}
 }
