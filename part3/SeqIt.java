3e9af8d9c0dcb31630657194cfceb6808ddf56b5
galew <lew.garrett@gmail.com>
2014-02-09 17:41:58 -0800

    Update SeqIt.java
    not complete

--- part3/SeqIt.java
+++ part3/SeqIt.java
@@ -6,3 +6,83 @@ public interface SeqIt {
     // return the next element and advance iterator to following item.
     public int next();
 }
+
+protected class ConstantIt extends SeqIt {
+	protected int it;
+	public ConstantIt(Constant s) {
+		it = 0;
+	}
+	
+	@Override public boolean hasNext() {
+		if (it != s.n)
+			return true;
+		else
+			return false;
+	}
+
+	@Override public int next() {
+		return v;
+		if (hasNext() == false) {
+			System.err;
+			System.exit(1);
+		}
+		else
+			it++;
+	}
+}
+
+protected class DeltaIt extends SeqIt {
+	protected int it;
+	public DeltaIt(Delta s) {
+		it = 0;
+	}
+	
+	@Override public boolean hasNext() {
+		if (it != s.num)
+			return true;
+		else
+			return false;
+	}
+	
+	@Override public int next() {
+		if (hasNext() == false) {
+			System.err.println("No next value")
+			System.exit(1);
+		}
+		else {
+			int temp;
+			temp = it * s.d;
+			temp = init + temp;
+			return temp;
+		}
+	}
+}
+
+protected class JumbleIt extends SeqIt {
+	protected int it;
+	public JumbleIt(Jumble s) {
+		it = 0;
+	}
+	
+	@Override public boolean hasNext() {
+		if (it != s.length)
+			return true;
+		else
+			return false;
+	}
+	
+	@Override public int next() {
+		if (hasNext() == false() {
+			System.err.println("No next value");
+			System.exit(1);
+		else {
+			return s.val[it];
+			it++
+		}
+	}
+}
+
+protected class JumbleUser extends JumbleIt {
+	public static int lengthLongestNDCSS1(Jumble j);
+	
+}
