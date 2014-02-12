47619bb98e4db66a18e5a38a361ba4333fe9a790
galew <lew.garrett@gmail.com>
2014-02-11 21:09:19 -0800

    debug update

--- part3/SeqIt.java
+++ part3/SeqIt.java
@@ -7,23 +7,25 @@ public interface SeqIt {
     public int next();
 }
 
-protected class ConstantIt extends SeqIt {
+public class ConstantIt extends SeqIt {
 	protected int it;
+	public Constant seq;
 	public ConstantIt(Constant s) {
+		seq = s;
 		it = 0;
 	}
 	
-	@Override public boolean hasNext() {
-		if (it != s.n)
+	public boolean hasNext() {
+		if (it != seq.n)
 			return true;
 		else
 			return false;
 	}
 
-	@Override public int next() {
-		return v;
+	public int next() {
+		return seq.v;
 		if (hasNext() == false) {
-			System.err;
+			System.err.println("No next value");
 			System.exit(1);
 		}
 		else
@@ -31,75 +33,82 @@ protected class ConstantIt extends SeqIt {
 	}
 }
 
-protected class DeltaIt extends SeqIt {
+public class DeltaIt extends SeqIt {
 	protected int it;
+	public Delta seq;
 	public DeltaIt(Delta s) {
+		seq = s;
 		it = 0;
 	}
 	
-	@Override public boolean hasNext() {
-		if (it != s.num)
+	public boolean hasNext() {
+		if (it != seq.num)
 			return true;
 		else
 			return false;
 	}
 	
-	@Override public int next() {
+	public int next() {
 		if (hasNext() == false) {
-			System.err.println("No next value")
+			System.err.println("No next value");
 			System.exit(1);
 		}
 		else {
 			int temp;
-			temp = it * s.d;
-			temp = init + temp;
+			temp = it * seq.d;
+			temp = seq.init + temp;
 			return temp;
 		}
 	}
 }
 
-protected class JumbleIt extends SeqIt {
+public class JumbleIt extends SeqIt {
 	protected int it;
+	public Jumble seq;
 	public JumbleIt(Jumble s) {
+		seq = s;
 		it = 0;
 	}
 	
-	@Override public boolean hasNext() {
-		if (it != s.length)
+	public boolean hasNext() {
+		if (it != seq.length)
 			return true;
 		else
 			return false;
 	}
 	
-	@Override public int next() {
-		if (hasNext() == false() {
+	public int next() {
+		if (hasNext() == false) {
 			System.err.println("No next value");
 			System.exit(1);
+		}
 		else {
-			return s.val[it];
+			return seq.val[it];
 			it++
 		}
 	}
 }
 
-protected class JumbleUser extends JumbleIt {
+public class JumbleUser extends JumbleIt {
+	
 	public static int lengthLongestNDCSS1(Jumble j) {
 	int temp = 1;
 	int longest = 0;
 
-	if(j.hasNext) {
+	if(j.hasNext()) {
 		int holder1 = j.next();
 		longest = temp;
 	}
 
-	while (j.hasNext) {
+	while (j.hasNext()) {
 		int holder2 = j.next();
 		if( holder2>= holder1)
 			temp++;
-		else:
+		else {
 			if(temp > longest)
 				longest = temp;
 			temp = 1;
+		}
 		holder1=holder2;
 
 
