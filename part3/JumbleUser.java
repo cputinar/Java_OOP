b69b342cb70669335c585b860ded5d15c06eeafd
galew <lew.garrett@gmail.com>
2014-02-11 21:30:28 -0800

    Update JumbleUser.java

--- part3/JumbleUser.java
+++ part3/JumbleUser.java
@@ -1,24 +1,26 @@
 public class JumbleUser implements JumbleIt {
-	
+	JumbleIt jseq;
 	public static int lengthLongestNDCSS1(Jumble j) {
 	int temp = 1;
 	int longest = 0;
-
-	if(j.hasNext()) {
-		int holder1 = j.next();
+	int holder1;
+	int holder2;
+	
+	if(jseq.hasNext()) {
+		holder1 = jseq.next();
 		longest = temp;
 	}
 
-	while (j.hasNext()) {
-		int holder2 = j.next();
-		if( holder2>= holder1)
+	while (jseq.hasNext()) {
+		holder2 = jseq.next();
+		if( holder2 >= holder1)
 			temp++;
 		else {
 			if(temp > longest)
 				longest = temp;
 			temp = 1;
 		}
-		holder1=holder2;
+		holder1 = holder2;
 
 
 	return longest;
