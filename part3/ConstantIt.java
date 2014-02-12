f2eb2b84aac5ed1cea3b08ec0147cce1db2a15ac
galew <lew.garrett@gmail.com>
2014-02-11 21:21:32 -0800

    Create ConstantIt.java

---
+++ part3/ConstantIt.java
@@ -0,0 +1,25 @@
+public class ConstantIt implements SeqIt {
+	protected int it;
+	public Constant seq;
+	public ConstantIt(Constant s) {
+		seq = s;
+		it = 0;
+	}
+	
+	public boolean hasNext() {
+		if (it != seq.n)
+			return true;
+		else
+			return false;
+	}
+
+	public int next() {
+		return seq.v;
+		if (hasNext() == false) {
+			System.err.println("No next value");
+			System.exit(1);
+		}
+		else
+			it++;
+	}
+}
